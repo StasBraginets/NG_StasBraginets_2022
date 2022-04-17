ee4a718b848245993d7e6849a3c4f52f3c28a2e3
Пользователь <braginets551@gmail.com>
2022-04-17 12:16:17 +0300

    Implemented program that display the picture

---
+++ Lesson_1/Task_2/task_2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    cout << "   ,    ," << endl
+         << "  /////|" << endl
+         << " ///// |" << endl
+         << "|~~~|  |" << endl
+         << "|===|  |" << endl
+         << "|j  |  |" << endl
+         << "| g |  |" << endl
+         << "|  s| /" << endl
+         << "|===|/" << endl
+         << "'---'" << endl;
+}
