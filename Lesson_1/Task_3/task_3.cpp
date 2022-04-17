fe7dff694084106c8065d6322585cd2d7c37c699
Пользователь <braginets551@gmail.com>
2022-04-17 12:40:51 +0300

    Implemented program that checks the age and output appropriate phrase

---
+++ Lesson_1/Task_3/task_3.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    short age;
+
+    cout << "Enter your age: ";
+    cin >> age;
+
+    if (age > 10)
+    {
+        short answer;
+
+        cout << "Do you study? (1/0): ";
+        cin >> answer;
+
+        if (answer == 1)
+        {
+            cout << "You are awesome!" << endl;
+        }
+        else if (answer == 0)
+        {
+            cout << "Why?" << endl;
+        }
+    }
+    else if (age > 0 && age <= 10)
+    {
+        cout << "You are already so big!" << endl;
+    }
+    else
+    {
+        cout << "Incorrect input!!" << endl;
+    }
+
+    return 0;
+}
