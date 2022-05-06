40f42ffbc609f6792818ee32925ed05e2c77a13f
Пользователь <braginets551@gmail.com>
2022-05-06 13:38:58 +0300

    Did the task without string libraries

--- Lesson_4/Task_1/task_1.cpp
+++ Lesson_4/Task_1/task_1.cpp
@@ -1,6 +1,5 @@
 // Which of two string is bigger
 #include <iostream>
-#include <cstring>
 #define Size 100
 
 using namespace std;
@@ -14,10 +13,28 @@ int main()
     cout << "Enter the second string!!" << endl;
     cin.getline(str2, Size);
 
+    // The length of the first string
+    short count_str1 = 0;
+    while (str1[count_str1] != '\0')
+    {
+        count_str1++;
+    }
+
+    // The length of the first string
+    short count_str2 = 0;
+    while (str2[count_str2] != '\0')
+    {
+        count_str2++;
+    }
+
     // Checking
-    if (strlen(str1) > strlen(str2))
+    if (count_str1 > count_str2)
     {
         cout << "The first string is bigger!" << endl;
+    }
+    else if (count_str1 == count_str2)
+    {
+        cout << "They are equal!" << endl;
     }
     else
     {
