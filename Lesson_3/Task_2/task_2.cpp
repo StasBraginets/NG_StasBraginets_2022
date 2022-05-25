2d2d747a46c17b2fe74cae82d624d037e3007ee0
Пользователь <braginets551@gmail.com>
2022-05-25 16:55:28 +0300

    Changed the name of constant variable

--- Lesson_3/Task_2/task_2.cpp
+++ Lesson_3/Task_2/task_2.cpp
@@ -3,24 +3,24 @@ Elements of the first array add to elements of the second array*/
 #include <iostream>
 #include <ctime>
 
-#define Size 4
+#define SIZE 4
 int main(void)
 {
     using namespace std;
 
-    int arr_num1[Size], arr_num2[Size];
+    int arr_num1[SIZE], arr_num2[SIZE];
 
     // random number generation
     srand(time(NULL));
     cout << "Your first array is: ";
-    for (short i = 0; i < Size; i++)
+    for (short i = 0; i < SIZE; i++)
     {
         arr_num1[i] = rand() % 10;
         cout << arr_num1[i] << " ";
     }
     cout << endl;
     cout << "Your second array is: ";
-    for (short i = 0; i < Size; i++)
+    for (short i = 0; i < SIZE; i++)
     {
         arr_num2[i] = rand() % 10;
         cout << arr_num2[i] << " ";
@@ -29,7 +29,7 @@ int main(void)
 
     //The elements of array to integer number
     short x = 0, y = 0;
-    for (short i = Size-1, j = 1; i >= 0; i--, j *= 10)
+    for (short i = SIZE-1, j = 1; i >= 0; i--, j *= 10)
     {
         x += arr_num1[i] * j;
         y += arr_num2[i] * j;
